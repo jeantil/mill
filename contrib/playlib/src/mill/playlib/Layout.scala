2a6e8ffeb9df71a8ab50f3cb677a041de83d8473
Jean Helou <jhe@codamens.fr>
2019-02-15 11:41:36 +0100

    Adds individual modules to configure mill to playframework's defaults
    This adds :
    - version configuration
    - dependencies
    - directory layout
    - runtime server

---
+++ contrib/playlib/src/mill/playlib/Layout.scala
@@ -0,0 +1,18 @@
+package mill
+package playlib
+
+import mill.scalalib._
+
+private[playlib] trait Layout extends JavaModule{
+
+
+  def conf = T.sources{ millSourcePath / 'conf }
+  def app = T.sources{ millSourcePath / 'app }
+
+  override def sources = T.sources{ app() }
+  override def resources = T.sources{ conf() }
+}
+
+
+
+
